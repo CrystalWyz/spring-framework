c857a79c2abf57022f514b008cd8ef791d1f47f3
wangnanxiang <wangnanxiang@huice.com>
2022-03-05 12:52:02 +0800

    spring 源码注释

--- spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
@@ -177,7 +177,9 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader
 	/** Logger used by this class. Available to subclasses. */
 	protected final Log logger = LogFactory.getLog(getClass());
 
-	/** Unique id for this context, if any. */
+	/** Unique id for this context, if any.
+	 *  创建上下文唯一标识
+	 */
 	private String id = ObjectUtils.identityToString(this);
 
 	/** Display name. */
@@ -242,8 +244,10 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader
 
 	/**
 	 * Create a new AbstractApplicationContext with no parent.
+	 * 创建一个无父类的AbstractApplicationContext对象
 	 */
 	public AbstractApplicationContext() {
+		// 创建资源模式处理器
 		this.resourcePatternResolver = getResourcePatternResolver();
 	}
 
@@ -484,6 +488,7 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader
 	 * @see org.springframework.core.io.support.PathMatchingResourcePatternResolver
 	 */
 	protected ResourcePatternResolver getResourcePatternResolver() {
+		// 创建一个资源模式解析器(其实就是用来解析xml配置文件)
 		return new PathMatchingResourcePatternResolver(this);
 	}
 
